2c5cde1e75a83de995cbe4850ce91e7c6cdb4297
Intrepid Panda <jerick999@gmail.com>
2018-12-01 16:56:51 -0700

    Change color displays to be consistent, set up classes to make it easier to lock in favorite colors

--- public/scripts.js
+++ public/scripts.js
@@ -5,20 +5,12 @@ const repopulatePalette = () => {
     colors.push('#' + Math.floor(Math.random() * 16777216).toString(16))
   }
 
-  document.querySelectorAll('.first-color').forEach(item => {
-    item.style.backgroundColor = colors[0];
-  });
-  document.querySelectorAll('.second-color').forEach(item => {
-    item.style.backgroundColor = colors[1];
-  });
-  document.querySelectorAll('.third-color').forEach(item => {
-    item.style.backgroundColor = colors[2];
-  });
-  document.querySelectorAll('.fourth-color').forEach(item => {
-    item.style.backgroundColor = colors[3];
-  });
-  document.querySelectorAll('.fifth-color').forEach(item => {
-    item.style.backgroundColor = colors[4];
+  colors.forEach((color, index) => {
+    if (color is not locked) {
+      document.querySelectorAll('.color-' + index).forEach(item => {
+        item.style.backgroundColor = colors[0];
+      });
+    }
   });
 }
 
