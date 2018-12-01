a4496258d90a2e9c107da883662cb8cfb6958c86
Intrepid Panda <jerick999@gmail.com>
2018-12-01 16:46:02 -0700

    Change implementation of setting new colors in place of old ones

--- public/scripts.js
+++ public/scripts.js
@@ -1,46 +1,25 @@
 const repopulatePalette = () => {
-  const palette = document.querySelectorAll('.color');
-  const contrastSection = document.querySelectorAll('.contrast-color');
   const colors = [];
 
-  palette.forEach((item, index) => {
-    const newColor = Math.floor(Math.random() * 16777216).toString(16);
-
-    colors.push(newColor);
-    setPaletteItemStyle(item, newColor);
-  });
-
-  contrastSection.forEach((item, index) => {
-    const currentColor = colors[index];
-    const newColors = colors.filter(color => {
-      return color !== currentColor;
-    });
-
-    setContrastItemStyle(item, currentColor, newColors);
-  });
-}
-
-const getTextColor = (string) => {
-  const r = parseInt(string.slice(0,2), 16);
-  const g = parseInt(string.slice(2,4), 16);
-  const b = parseInt(string.slice(4), 16);
-
-  if (((r * 0.21) + (g * 0.72) + (b * 0.07)) >= 125) {
-    return '0a140f';
-  } else {
-    return 'e1e1e1';
+  while (colors.length < 5) {
+    colors.push('#' + Math.floor(Math.random() * 16777216).toString(16))
   }
-}
 
-const setPaletteItemStyle = (item, newColor) => {
-  item.style.backgroundColor = '#' + newColor;
-  item.style.color = '#' + getTextColor(newColor);
-  item.querySelector('h2').innerText = '#' + newColor.toUpperCase();
-}
-
-const setContrastItemStyle = (item, currentColor, newColors) => {
-  item.style.backgroundColor = '#' + currentColor;
-  item.style.boxShadow = `2.5rem 2.5rem #${newColors[0]}, -2.5rem 2.5rem #${newColors[1]}, -2.5rem -2.5rem #${newColors[2]}, 2.5rem -2.5rem #${newColors[3]}`;
+  document.querySelectorAll('.first-color').forEach(item => {
+    item.style.backgroundColor = colors[0];
+  });
+  document.querySelectorAll('.second-color').forEach(item => {
+    item.style.backgroundColor = colors[1];
+  });
+  document.querySelectorAll('.third-color').forEach(item => {
+    item.style.backgroundColor = colors[2];
+  });
+  document.querySelectorAll('.fourth-color').forEach(item => {
+    item.style.backgroundColor = colors[3];
+  });
+  document.querySelectorAll('.fifth-color').forEach(item => {
+    item.style.backgroundColor = colors[4];
+  });
 }
 
 document.querySelector('.gnp').addEventListener('click', repopulatePalette);
