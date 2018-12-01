8e9da50a98dde224649299ad562b207c83fb4b70
Intrepid Panda <jerick999@gmail.com>
2018-12-01 16:11:41 -0700

    Implement changing box shadows with palette change

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,22 @@
 const repopulatePalette = () => {
   const palette = document.querySelectorAll('.color');
   const contrastSection = document.querySelectorAll('.contrast-color');
+  const colors = [];
 
   palette.forEach((item, index) => {
     const newColor = Math.floor(Math.random() * 16777216).toString(16);
-    const textColor = getTextColor(newColor);
-    item.style.backgroundColor = '#' + newColor;
-    item.style.color = '#' + textColor;
-    item.querySelector('h2').innerText = '#' + newColor.toUpperCase();
+
+    colors.push(newColor);
+    setPaletteItemStyle(item, newColor);
+  });
+
+  contrastSection.forEach((item, index) => {
+    const currentColor = colors[index];
+    const newColors = colors.filter(color => {
+      return color !== currentColor;
+    });
+
+    setContrastItemStyle(item, currentColor, newColors);
   });
 }
 
@@ -15,7 +24,7 @@ const getTextColor = (string) => {
   const r = parseInt(string.slice(0,2), 16);
   const g = parseInt(string.slice(2,4), 16);
   const b = parseInt(string.slice(4), 16);
-  
+
   if (((r * 0.21) + (g * 0.72) + (b * 0.07)) >= 125) {
     return '0a140f';
   } else {
@@ -23,5 +32,15 @@ const getTextColor = (string) => {
   }
 }
 
+const setPaletteItemStyle = (item, newColor) => {
+  item.style.backgroundColor = '#' + newColor;
+  item.style.color = '#' + getTextColor(newColor);
+  item.querySelector('h2').innerText = '#' + newColor.toUpperCase();
+}
+
+const setContrastItemStyle = (item, currentColor, newColors) => {
+  item.style.backgroundColor = '#' + currentColor;
+  item.style.boxShadow = `2.5rem 2.5rem #${newColors[0]}, -2.5rem 2.5rem #${newColors[1]}, -2.5rem -2.5rem #${newColors[2]}, 2.5rem -2.5rem #${newColors[3]}`;
+}
 
 document.querySelector('.gnp').addEventListener('click', repopulatePalette);
