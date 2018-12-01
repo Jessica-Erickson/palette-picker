aa2d4a2261a20e170fdcda603466a56052da6117
Intrepid Panda <jerick999@gmail.com>
2018-12-01 15:27:06 -0700

    Remove unnecessary classes in html, tighten up text color conditional, set up setting new color to contrast boxes

--- public/scripts.js
+++ public/scripts.js
@@ -1,23 +1,27 @@
 const repopulatePalette = () => {
   const palette = document.querySelectorAll('.color');
-  palette.forEach(item => {
+  const contrastSection = document.querySelectorAll('.contrast-color');
+
+  palette.forEach((item, index) => {
     const newColor = Math.floor(Math.random() * 16777216).toString(16);
-    const textColor = getInvertedColor(newColor);
+    const textColor = getTextColor(newColor);
     item.style.backgroundColor = '#' + newColor;
     item.style.color = '#' + textColor;
     item.querySelector('h2').innerText = '#' + newColor.toUpperCase();
   });
 }
 
-const getInvertedColor = (string) => {
+const getTextColor = (string) => {
   const r = parseInt(string.slice(0,2), 16);
   const g = parseInt(string.slice(2,4), 16);
   const b = parseInt(string.slice(4), 16);
-  if (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 125) {
+  
+  if (((r * 0.21) + (g * 0.72) + (b * 0.07)) >= 125) {
     return '0a140f';
   } else {
     return 'e1e1e1';
   }
 }
+
 
 document.querySelector('.gnp').addEventListener('click', repopulatePalette);
