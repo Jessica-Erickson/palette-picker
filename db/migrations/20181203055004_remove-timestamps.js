0d11b53410f0affec1afc8695f5e57a4d9a0a0c8
Intrepid Panda <jerick999@gmail.com>
2018-12-03 05:58:32 -0700

    Remove timestamps from tables because I don't need them

---
+++ db/migrations/20181203055004_remove-timestamps.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('projects', function(table) {
+      table.dropTimestamps();
+    }),
+    knex.schema.table('palettes', function(table) {
+      table.dropTimestamps();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('papers', function(table) {
+      table.timestamps(true, true);
+    }),
+    knex.schema.table('palettes', function(table) {
+      table.timestamps(true, true);
+    })
+  ]);
+};
