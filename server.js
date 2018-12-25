08d48cddb88c1a5443730e7e3e7f81992adf06d5
Intrepid Panda <jerick999@gmail.com>
2018-12-25 15:53:45 -0700

    Implement duplicate rejection at post projects endpoint

--- server.js
+++ server.js
@@ -25,13 +25,26 @@ app.post('/api/v1/projects', (request, response) => {
   const { name } = request.body;
 
   database('projects')
-    .insert({ name }, 'id')
-    .then(project_id => {
-      response.status(201).send({ message: `Success! Project ${name} has been stored and given id number ${project_id[0]}` });
+    .select()
+    .then(projects => {
+      const exists = projects.some(project => project.name === name);
+      if (exists) {
+        return response.status(409).send({ message: `Failure: Project ${name} already exists.`});
+      } else {
+        database('projects')
+          .insert({ name }, 'id')
+          .then(project_id => {
+            response.status(201).send({ message: `Success! Project ${name} has been stored and given id number ${project_id[0]}` });
+          })
+          .catch(error => {
+            response.status(500).send({ error });
+          });
+      }
     })
     .catch(error => {
       response.status(500).send({ error });
-    });
+    })
+
 });
 
 app.get('/api/v1/projects/:id', (request, response) => {
