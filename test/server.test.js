7b844c4afbb297f91596b6df4aad20d588097f5e
Intrepid Panda <jerick999@gmail.com>
2018-12-25 14:41:56 -0700

    Write test for get all projects endpoint

--- test/server.test.js
+++ test/server.test.js
@@ -12,7 +12,25 @@ describe('Palette Picker API V1', () => {
   beforeEach(done => {
     database.migrate.rollback()
       .then(() => database.migrate.latest())
-      .then(() => database.seed.run());
+      .then(() => database.seed.run())
       .then(() => done());
+  });
+
+  describe('Get all projects from /api/v1/projects', () => {
+    it('should respond with a status of 200 and an array of existing projects', done => {
+      chai.request(app)
+        .get('/api/v1/projects')
+        .end((error, response) => {
+          expect(response).to.have.status(200);
+          expect(response).to.be.json;
+          expect(response.body).to.be.an('array');
+          expect(response.body).to.have.length(3);
+          for (let i = 0; i < 3; i++) {
+            expect(response.body[i]).to.have.property('name');
+            expect(response.body[i].name).to.not.be.undefined;
+          }
+          done();
+        });
+    });
   });
 });
